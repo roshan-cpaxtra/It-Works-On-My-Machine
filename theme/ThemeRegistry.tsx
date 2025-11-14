2e3a712c31fb0d53bdcc77da1672054f2cbff6fa
Roshan Pandey <rpandey@cpaxtra.co.th>
2025-11-14 11:48:53 +0700

    Add mui-nextjs

--- theme/ThemeRegistry.tsx
+++ theme/ThemeRegistry.tsx
@@ -3,13 +3,16 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import theme from './theme';
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
   );
 }
